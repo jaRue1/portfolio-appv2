16bddefe566c0323342fa77d195361f1cda931c3
jaRue1 <jaruejohnson@outlook.com>
2021-11-10 21:44:47 -0500

    completed intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react"
 import "./intro.scss"
+import { init } from "ityped"
 function Intro() {
+  const textRef = useRef()
+
+  useEffect(() => {
+    init(textRef.current, {
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true,
+      strings: ["Developer", "Designer", "Music Producer"],
+    })
+  }, [])
   return (
-    <div className='intro' id='intro'>
-      
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/r3.jpg"></img>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Jarue Johnson</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" />
+        </a>
+      </div>
     </div>
   )
 }
